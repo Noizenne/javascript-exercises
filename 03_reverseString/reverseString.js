cc03569f6c8bf627d1806d89d3db8e1970d529f6
Noizenne <73695367+Noizenne@users.noreply.github.com>
2024-03-17 18:40:42 +0100

    Completed up to 3 tasks

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    const newWord = word.split("").reverse().join("");
+    return newWord;
 };
-
+reverseString("hello there");
 // Do not edit below this line
 module.exports = reverseString;
