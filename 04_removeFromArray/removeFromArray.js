4a531fca3d91e4821dc11237bb6ec4ee16af3571
Noizenne <73695367+Noizenne@users.noreply.github.com>
2024-03-18 20:25:54 +0100

    Did task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    newArr = [];
 
+    arr.forEach(element => {
+        if(!args.includes(element)) {
+            newArr.push(element);
+        }
+    });
+
+    return newArr;
 };
+
+removeFromArray([1,2,3,4], 2, 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
