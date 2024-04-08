ceb64918eb2d28ae7f0c612c15f50ca81128a2e2
Noizenne <73695367+Noizenne@users.noreply.github.com>
2024-04-08 20:32:25 +0200

    Did last task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(n) {
+  const formula = (n - 32) * (5 / 9);
+  const fixed = parseFloat(formula.toFixed(1));
+  return fixed;
 };
 
-const convertToFahrenheit = function() {
+convertToCelsius(32)
+
+const convertToFahrenheit = function(n) {
+  const formula = n * 9 / 5 + 32;
+  const fixed = parseFloat(formula.toFixed(1));
+  return fixed;
 };
+
+convertToFahrenheit(0)
 
 // Do not edit below this line
 module.exports = {
